7d38a3b12f2c76d87bb3ba6781083fb42a61b81c
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-07 22:38:56 +0200

    Add single cards

--- script/script.js
+++ script/script.js
@@ -1,38 +1,39 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
 
 Book.prototype.info = function () {
   let hasRead = this.read ? "already" : "never"
-  return `${this.title} ${this.author} has ${this.pages} pages ${hasRead} read this book` 
+  return `${this.title} ${this.author} has ${this.pages} pages ${hasRead} read this book`
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
+const displayBooks = (library) => {
+  let selector = document.getElementById("show");
+  for (let i = 0; i < library.length; i++) {
+
+  }
+}
+
 let addBtn = document.getElementById("add-book")
 
 addBtn.addEventListener('click', function () {
+  let selector = document.getElementById("show");
   const book1 = new Book('Star Wars', 'George Lucas', 1000, false)
   addBookToLibrary(book1)
   console.log(myLibrary)
-
-  let selector = document.getElementById("show");
-  for (let i=0; i<myLibrary.length; i++) {
+  displayBooks(myLibrary)
   let paragraph = document.createElement("p")
-  paragraph.innerHTML = myLibrary[i].info()
-  
+  paragraph.innerHTML = book1.info()
   let newBook = document.createElement("div")
   newBook.appendChild(paragraph)
   selector.appendChild(newBook);
-}
-
 })
-
-
