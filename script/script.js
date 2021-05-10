9fe154a1c8b8e243536528b542da1745058170de
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-10 22:17:19 +0200

    Handle remove buttons

--- script/script.js
+++ script/script.js
@@ -1,5 +1,22 @@
 let myLibrary = [];
 let selector = document.getElementById("show");
+let mainDivCard = document.createElement("div")
+let imageCard = document.createElement("img")
+let cardBody = document.createElement("div")
+let cardTile = document.createElement("h5")
+let cardAuthor = document.createElement("p")
+let cardDescription = document.createElement("em")
+let readBtn = document.createElement("button")
+let icon = document.createElement("i")
+
+const handleRemove = (index) => {
+  myLibrary.splice(index, 1)
+  while (selector.firstChild) {
+    selector.removeChild(selector.firstChild);
+  }
+
+  console.log(myLibrary)
+}
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -17,76 +34,84 @@ function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
-const displayBooks = (library) => {
-  let selector = document.getElementById("show");
-  for (let i = 0; i < library.length; i++) {
-
-  }
-}
-
-let addBtn = document.getElementById("add-book")
-
-addBtn.addEventListener('click', function () {
-  console.log(document.getElementById("read").checked);
+const displayBooks = () => {
   let title = document.getElementById("title").value
   let author = document.getElementById("author").value
   let pages = document.getElementById("pages").value
   let readCheck = document.getElementById("read").checked == true ? "Mark as unreaded" : "Mark as readed"
 
   const book = new Book(title, author, pages, readCheck)
-
   addBookToLibrary(book)
-  console.log(myLibrary)
+  console.log(myLibrary);
 
-  let icon = document.createElement("i")
-  icon.classList.add("far")
-  icon.classList.add("fa-trash-alt")
-  icon.classList.add("text-light")
-  let mainDivCard = document.createElement("div")
-  let imageCard = document.createElement("img")
-  let cardBody = document.createElement("div")
-  let cardTile = document.createElement("h5")
-  let cardAuthor = document.createElement("p")
-  let cardDescription = document.createElement("em")
-  let readBtn = document.createElement("button")
-  readBtn.classList.add("btn")
-  readBtn.classList.add("btn-primary")
-  readBtn.innerHTML = readCheck
+  while (selector.firstChild) {
+    selector.removeChild(selector.firstChild);
+  }
+  // selector has zero children
+  for (let book in myLibrary) {
+    // let icon = document.createElement("i")
+    icon.classList.add("far")
+    icon.classList.add("fa-trash-alt")
+    icon.classList.add("text-light")
+    // let mainDivCard = document.createElement("div")
+    // let imageCard = document.createElement("img")
+    // let cardBody = document.createElement("div")
+    // let cardTile = document.createElement("h5")
+    // let cardAuthor = document.createElement("p")
+    // let cardDescription = document.createElement("em")
+    // let readBtn = document.createElement("button")
+    readBtn.classList.add("btn")
+    readBtn.classList.add("btn-primary")
+    readBtn.innerHTML = readCheck
 
-  let readBtn2 = document.createElement("button")
-  readBtn2.classList.add("btn")
-  readBtn2.classList.add("btn-primary")
-  readBtn2.classList.add("mx-1")
-  readBtn2.appendChild(icon)
+    let removeButton = document.createElement("button")
+    removeButton.classList.add("btn")
+    removeButton.classList.add("btn-primary")
+    removeButton.classList.add("mx-1")
+    removeButton.appendChild(icon)
+    removeButton.id = myLibrary.length - 1
 
-  cardTile.classList.add("card-title")
-  cardAuthor.classList.add("card-text")
-  imageCard.classList.add("card-img-top")
-  imageCard.src="https://miro.medium.com/max/820/1*1uTX2gdtlj9gsKxbiOc6LQ.jpeg"
-  imageCard.alt = "Card image"
-  cardTile.innerHTML = book.title
-  cardDescription.innerHTML = `Status: ${book.pages} pages ${readCheck ? `Readed` : `Unreaded`}`
-  cardAuthor.innerHTML = book.author
-  cardBody.classList.add("card-body")
-  cardBody.appendChild(cardTile)
-  cardBody.appendChild(cardAuthor)
-  cardBody.appendChild(cardDescription)
-  cardBody.appendChild(readBtn)
-  cardBody.appendChild(readBtn2)
+    removeButton.addEventListener("click", function (event) {
+      handleRemove(removeButton.id)
+    })
 
-  mainDivCard.classList.add("card")
-  mainDivCard.classList.add("col-3")
-  mainDivCard.classList.add("mx-3")
-  mainDivCard.classList.add("my-3")
+    cardTile.classList.add("card-title")
+    cardAuthor.classList.add("card-text")
+    imageCard.classList.add("card-img-top")
+    imageCard.src = "https://miro.medium.com/max/820/1*1uTX2gdtlj9gsKxbiOc6LQ.jpeg"
+    imageCard.alt = "Card image"
+    cardTile.innerHTML = book.title
+    cardDescription.innerHTML = `Status: ${book.pages} pages ${readCheck ? `Readed` : `Unreaded`}`
+    cardAuthor.innerHTML = book.author
+    cardBody.classList.add("card-body")
+    cardBody.appendChild(cardTile)
+    cardBody.appendChild(cardAuthor)
+    cardBody.appendChild(cardDescription)
+    cardBody.appendChild(readBtn)
+    cardBody.appendChild(removeButton)
 
-  mainDivCard.appendChild(imageCard)
-  mainDivCard.appendChild(cardBody)
+    mainDivCard.classList.add("card")
+    mainDivCard.classList.add("col-3")
+    mainDivCard.classList.add("mx-3")
+    mainDivCard.classList.add("my-3")
 
-  selector.appendChild(mainDivCard);
-  document.getElementById("title").value = ""
-  document.getElementById("author").value = ""
-  document.getElementById("pages").value = ""
-  document.getElementById("read").checked = false
+    mainDivCard.appendChild(imageCard)
+    mainDivCard.appendChild(cardBody)
+
+    selector.appendChild(mainDivCard);
+    document.getElementById("title").value = ""
+    document.getElementById("author").value = ""
+    document.getElementById("pages").value = ""
+    document.getElementById("read").checked = false
+  }
+
+}
+
+let addBtn = document.getElementById("add-book")
+
+addBtn.addEventListener('click', function () {
+  // console.log(myLibrary)
+  displayBooks(myLibrary)
   alert("Books added to the library successfully");
 
 })
